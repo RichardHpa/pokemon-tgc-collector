02ba25ca0ff4e41015e7f6e6510184ce1a0fb593
Richard <richard.m.hpa@gmail.com>
2022-10-21 13:49:42 +1300

    added case for first release

--- .github/actions/compare-versions/index.js
+++ .github/actions/compare-versions/index.js
@@ -18,22 +18,27 @@ async function run() {
       per_page: 1,
     });
 
-    const latestVersion = semver.parse(releases[0].tag_name);
-    if (!latestVersion?.version) {
-      throw new Error(`Could not parse a version from octokit api.`);
+    if (releases.length === 0) {
+      core.setOutput("isNewerVersion", true);
+      core.setOutput("versionType", "first-release");
+    } else {
+      const latestVersion = semver.parse(releases[0].tag_name);
+      if (!latestVersion?.version) {
+        throw new Error(`Could not parse a version from octokit api.`);
+      }
+
+      const isNewerVersion = semver.gt(
+        currentVersion.version,
+        latestVersion.version
+      );
+      core.setOutput("isNewerVersion", isNewerVersion);
+
+      const versionType = semver.diff(
+        currentVersion.version,
+        latestVersion.version
+      );
+      core.setOutput("versionType", versionType);
     }
-
-    const isNewerVersion = semver.gt(
-      currentVersion.version,
-      latestVersion.version
-    );
-    core.setOutput("isNewerVersion", isNewerVersion);
-
-    const versionType = semver.diff(
-      currentVersion.version,
-      latestVersion.version
-    );
-    core.setOutput("versionType", versionType);
   } catch (error) {
     core.setFailed(error.message);
   }
