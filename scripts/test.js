d365876a51ab161892474d4ad1c175e01cb33ec6
Richard <richard.m.hpa@gmail.com>
2022-11-11 22:42:08 +1300

    testing pre-commit

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,3 @@
-const spawnSync = require("child_process").spawnSync;
-
 const styles = {
   success: { open: "\u001b[32;1m", close: "\u001b[0m" },
   danger: { open: "\u001b[31;1m", close: "\u001b[0m" },
@@ -10,29 +8,29 @@ const styles = {
 function color(modifier, string) {
   return styles[modifier].open + string + styles[modifier].close;
 }
+
+const concurrently = require("concurrently");
+
 console.log(color("info", "▶️  Starting pre commit..."));
 
-const error = spawnSync("npx --version", { shell: true })
-  .stderr.toString()
-  .trim();
-if (error) {
-  console.error(
-    color(
-      "danger",
-      "🚨  npx is not available on this computer. Please install npm@5.2.0 or greater"
-    )
-  );
-  throw error;
-}
-
-const command = `npx concurrently --kill-others-on-fail --prefix "[{name}]" --names "test,lint,typecheck,build"   --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white" "npm run test --silent -- --watch=false" "npm run lint --silent" "npm run typecheck --silent" "npm run build --silent"`;
-console.log(color("subtitle", "      Running commands"));
-
-const result = spawnSync(command, { stdio: "inherit", shell: true });
-if (result.status === 0) {
-  console.log(color("success", "✅  Pre commit complete"));
-} else {
-  console.clear();
-  console.log(result.stdout);
-  // process.exit(resuxlt.status);
-}
+const { result } = concurrently(
+  [
+    { command: "npm run lint --silent", name: "lint" },
+    { command: "npm run test --silent -- --watch=false", name: "test" },
+    { command: "npm run typecheck", name: "typecheck" },
+    { command: "npm run build --silent", name: "build" },
+  ],
+  {
+    prefix: "name",
+    killOthers: ["failure"],
+    prefixColors:
+      "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white",
+  }
+);
+result
+  .then((res) => {
+    console.log(color("success", "✅  Pre commit complete"));
+  })
+  .catch((err) => {
+    console.error(color("danger", "🚨 Something needs fixing 🚨"));
+  });
