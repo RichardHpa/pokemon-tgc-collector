45041d8d99200c71e289018473ff72551a4d808d
Richard <richard.m.hpa@gmail.com>
2022-11-18 22:16:36 +1300

    created infinate scrolling for sets

--- app/api/getExpansions/getExpansions.tsx
+++ app/api/getExpansions/getExpansions.tsx
@@ -3,18 +3,19 @@ import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
 export interface getExpansionsProps {
   pageSize?: number;
   orderBy?: string;
+  page?: number;
 }
 
 export const getExpansions = async ({
   pageSize = 3,
   orderBy = "",
+  page = 1,
 }: getExpansionsProps) => {
   const sets = PokemonTCG.findSetsByQueries({
     pageSize,
     orderBy,
+    page,
   }).then((res) => res);
 
   return sets;
 };
-
-// -releaseDate;
--- app/routes/expansions/index.tsx
+++ app/routes/expansions/index.tsx
@@ -1,30 +1,111 @@
-import { useLoaderData } from "@remix-run/react";
+import { useEffect, useState, useCallback } from "react";
+import { useLoaderData, useFetcher } from "@remix-run/react";
 import { json } from "@remix-run/node";
 
 import { getExpansions } from "~/api/getExpansions";
 
 import { ExpansionCard } from "~/components/ExpansionCard";
 
+import type { Set } from "pokemon-tcg-sdk-typescript/dist/sdk";
 import type { LoaderFunction } from "@remix-run/node";
 
+// Pull page down from the loader's api request
+const getPage = (searchParams: URLSearchParams) =>
+  Number(searchParams.get("page") || "1");
+
 type LoaderData = {
-  data: Awaited<ReturnType<typeof getExpansions>>;
+  expansions: Awaited<ReturnType<typeof getExpansions>>;
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const page = getPage(new URL(request.url).searchParams);
+
+  const data = await getExpansions({
+    page,
+    pageSize: 12,
+    orderBy: "releaseDate",
+  });
+
   return json<LoaderData>({
-    data: await getExpansions({ orderBy: "releaseDate", pageSize: 12 }),
+    expansions: data,
   });
 };
 
 export default function ExpansionsIndexPage() {
-  const data = useLoaderData<LoaderData>();
+  const { expansions: initialExpansions } = useLoaderData<LoaderData>();
+  const [expansions, setExpansions] = useState(initialExpansions);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
+
+  const [height, setHeight] = useState(null);
+  const [shouldFetch, setShouldFetch] = useState(true);
+
+  // Start with two because 1 was pre-loaded
+  const [page, setPage] = useState(2);
+  const fetcher = useFetcher();
+
+  // Set height of the parent container whenever photos are loaded
+  const divHeight = useCallback(
+    (node: any) => {
+      if (node !== null) {
+        setHeight(node.getBoundingClientRect().height);
+      }
+    },
+    [expansions.length]
+  );
+
+  // Add Listeners to scroll and client resize
+  useEffect(() => {
+    const scrollListener = () => {
+      setClientHeight(window.innerHeight);
+      setScrollPosition(window.scrollY);
+    };
+
+    // Avoid running during SSR
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", scrollListener);
+    }
+
+    // Clean up
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", scrollListener);
+      }
+    };
+  }, []);
+
+  // Listen on scrolls. Fire on some self-described breakpoint
+  useEffect(() => {
+    if (!shouldFetch || !height) return;
+    if (clientHeight + scrollPosition + 100 < height) return;
+    fetcher.load(`/expansions?index&page=${page}`);
+
+    setShouldFetch(false);
+  }, [clientHeight, scrollPosition, fetcher]);
+
+  // Merge sets, increment page, and allow fetching again
+  useEffect(() => {
+    // Discontinue API calls if the last page has been reached
+    if (fetcher.data && fetcher.data.length === 0) {
+      setShouldFetch(false);
+      return;
+    }
+
+    if (fetcher.data && fetcher.data.expansions.length > 0) {
+      setExpansions((prevPhotos: Set[]) => [
+        ...prevPhotos,
+        ...fetcher.data.expansions,
+      ]);
+      setPage((page: number) => page + 1);
+      setShouldFetch(true);
+    }
+  }, [fetcher.data]);
 
   return (
-    <div>
-      {data?.data && (
+    <div ref={divHeight}>
+      {expansions && (
         <div className="grid grid-cols-4 gap-3" role="list">
-          {data?.data.map((set) => {
+          {expansions.map((set) => {
             return (
               <div
                 key={set.id}
